dc961e1f2dd9e8c225bc1a44441d32105123d439
himuravidal <cristian.vidal.lopez@gmail.com>
2017-09-06 18:27:19 -0300

    WIP:  some methods

--- prueba.rb
+++ prueba.rb
@@ -1,4 +1,32 @@
+
+def read_alum
 file = File.open('alumnos.cvs', 'r')
-alumnos = file.readlines.map(&:chomp).map{ |lines| lines.split(', ')}
+alumnos = file.readlines.map(&:chomp).map{ |lines| lines.split(', ') }
 file.close
-print alumnos
+alumnos
+end
+
+def clean_notes(alumnos)
+alumnos.each do |alum|
+  name = alum.shift
+  puts name
+end
+end
+
+
+
+def alum_prom(alumnos)
+  alumnos.each do |sin|
+
+
+sum = 0
+alumnos.each do |alum|
+  unless alum == 'A'
+    sum += alum.to_i
+  end
+end
+return sum / (alumnos.length.to_f)
+end
+end
+
+clean_notes(read_alum)
