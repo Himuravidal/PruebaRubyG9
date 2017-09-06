3a88eb16f226395ef7aaead6f982b1522fa68a25
himuravidal <cristian.vidal.lopez@gmail.com>
2017-09-06 19:18:10 -0300

    WIP: First Done :-)

--- prueba.rb
+++ prueba.rb
@@ -1,32 +1,41 @@
 
-def read_alum
-file = File.open('alumnos.cvs', 'r')
-alumnos = file.readlines.map(&:chomp).map{ |lines| lines.split(', ') }
+def read_alum(file_name)
+file = File.open(file_name, 'r')
+alumnos = file.readlines.map(&:chomp).map { |lines| lines.split(', ') }
 file.close
 alumnos
 end
 
-def clean_notes(alumnos)
-alumnos.each do |alum|
-  name = alum.shift
-  puts name
-end
-end
-
-
-
-def alum_prom(alumnos)
-  alumnos.each do |sin|
-
-
-sum = 0
-alumnos.each do |alum|
-  unless alum == 'A'
-    sum += alum.to_i
+# def clean_notes(alumnos)
+# alumnos.each do |alum|
+#   name = alum.shift
+#   alum
+# end
+# end
+def prom_alum(alum)
+  sum = 0
+  str_sum = 0
+  alum.each do |ele|
+    unless ele.to_i == 0
+      sum += ele.to_i
+    else
+      str_sum+=1
+    end
   end
-end
-return sum / (alumnos.length.to_f)
-end
+  return sum / (alum.length.to_f - str_sum)
 end
 
-clean_notes(read_alum)
+def add_alumno(file_name)
+  alumnos = read_alum(file_name)
+  # p alumnos
+  alumnos.each do |alumno|
+    nombre_alumno = alumno.shift
+    promedio = prom_alum(alumno)
+    File.open("promedios.txt", "a") { |file|
+		file.puts "#{nombre_alumno}: su promedio es #{promedio}"
+    }
+  end
+  return nil
+end
+
+add_alumno('alumnos.cvs')
