e3fed2db6762d5d2581ab90d32effe786be56712
himuravidal <cristian.vidal.lopez@gmail.com>
2017-09-06 19:30:06 -0300

    FIX: first methods with problem

--- prueba.rb
+++ prueba.rb
@@ -1,41 +1,30 @@
-
 def read_alum(file_name)
-file = File.open(file_name, 'r')
-alumnos = file.readlines.map(&:chomp).map { |lines| lines.split(', ') }
-file.close
-alumnos
+  file = File.open(file_name, 'r')
+  alumnos = file.readlines.map(&:chomp).map { |lines| lines.split(', ') }
+  file.close
+  alumnos
 end
 
-# def clean_notes(alumnos)
-# alumnos.each do |alum|
-#   name = alum.shift
-#   alum
-# end
-# end
 def prom_alum(alum)
   sum = 0
-  str_sum = 0
+  alt_sum = 0
   alum.each do |ele|
-    unless ele.to_i == 0
-      sum += ele.to_i
+    if ele.to_i.zero?
+      alt_sum += 1
     else
-      str_sum+=1
+      sum += ele.to_i
     end
   end
-  return sum / (alum.length.to_f - str_sum)
+  sum / (alum.length.to_f - alt_sum)
 end
 
 def add_alumno(file_name)
   alumnos = read_alum(file_name)
-  # p alumnos
   alumnos.each do |alumno|
-    nombre_alumno = alumno.shift
-    promedio = prom_alum(alumno)
-    File.open("promedios.txt", "a") { |file|
-		file.puts "#{nombre_alumno}: su promedio es #{promedio}"
-    }
+    alum_name = alumno.shift
+    prom = prom_alum(alumno)
+    File.open('promedios.txt', 'a') { |file| file.puts" #{alum_name} : su promedio es #{prom}" }
   end
-  return nil
 end
 
 add_alumno('alumnos.cvs')
